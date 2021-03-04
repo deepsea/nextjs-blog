d2c25fad0ecad3bf784aa76e1cdbf9c3b2dd2c9d
Richard Lawlor <lawlorichard@gmail.com>
2021-03-04 16:34:17 +0000

    Update update page
    Server side props example

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,49 @@
 import React from 'react'
 
-function about() {
+function about({profile}) {
     return (
         <div>
-            <h1>Hello</h1>
+            <h1>This is {profile.name}'s' profile page</h1>
+            <h2>He is {profile.age} years old</h2>
             
         </div>
     )
+}
+
+// Use this for Static, benefit of caching and quicker
+// export async function getStaticProps() {
+
+//     const data = {
+//         profile: {
+//             name: 'Richie',
+//             age: 99
+//         }
+//     }
+
+//     return {
+//         props: {
+//             profile: data.profile
+//         }
+//     }
+
+// }
+
+// use this for Server Side
+export async function getServerSideProps() {
+
+    const data = {
+        profile: {
+            name: 'Richie',
+            age: 99
+        }
+    }
+
+    return {
+        props: {
+            profile: data.profile
+        }
+    }
+
 }
 
 export default about
