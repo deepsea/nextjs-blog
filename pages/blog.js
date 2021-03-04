1c5ca0a70b89cedb9a52e4432be81f6e4b62faa6
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-03-04 17:48:37 +0100

    Add layout components. call it on the blog page

--- pages/blog.js
+++ pages/blog.js
@@ -1,47 +1,45 @@
-import React from 'react'
+import React from 'react';
+import Layout from '../components/layout';
 
 function blog({ posts }) {
-    return (
-        <div>
-            <h1>I am a blog</h1>
+  return (
+    <Layout>
+      <div>
+        <h1>I am a blog</h1>
 
-            {posts.map(post => (
-
-                <div key={post.id}>
-                    <h2>{post.title}</h2>
-                    <h3>By {post.author}</h3>
-                </div>
-
-            ))}
-            
-        </div>
-    );
+        {posts.map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <h3>By {post.author}</h3>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
 export async function getStaticProps() {
-    const res = {
-        data: [
-            {
-                id: '123',
-                title: 'My first blog',
-                author: 'Sonny'
-            },
-            {
-                id: '456',
-                title: 'Gems first blog',
-                author: 'Gwen'
-            }
-        ]
-    }
-    const posts = res.data
+  const res = {
+    data: [
+      {
+        id: '123',
+        title: 'My first blog',
+        author: 'Sonny',
+      },
+      {
+        id: '456',
+        title: 'Gems first blog',
+        author: 'Gwen',
+      },
+    ],
+  };
+  const posts = res.data;
 
-    return {
-        props: {
-            posts,
-
-        }
-    }
+  return {
+    props: {
+      posts,
+    },
+  };
 }
 
-
-export default blog
+export default blog;
