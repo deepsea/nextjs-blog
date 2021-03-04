3b65783c3ec5edef46485d8bc7d3fd7cb76d7c90
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-03-04 22:12:32 +0100

    Merge main

--- pages/about.js
+++ pages/about.js
@@ -1,13 +1,12 @@
-import React from 'react'
+import React from 'react';
 
-function about({profile}) {
+function about({ profile }) {
     return (
         <div>
             <h1>This is {profile.name}'s' profile page</h1>
             <h2>He is {profile.age} years old</h2>
-            
         </div>
-    )
+    );
 }
 
 // Use this for Static, benefit of caching and quicker
@@ -30,20 +29,18 @@ function about({profile}) {
 
 // use this for Server Side
 export async function getServerSideProps() {
-
     const data = {
         profile: {
             name: 'Richie',
-            age: 99
-        }
-    }
+            age: 99,
+        },
+    };
 
     return {
         props: {
-            profile: data.profile
-        }
-    }
-
+            profile: data.profile,
+        },
+    };
 }
 
-export default about
+export default about;
--- pages/blog.js
+++ pages/blog.js
@@ -1,86 +1,86 @@
 import Layout from '../components/layout';
-import Link from "next/link";
-import { Router, useRouter } from "next/router"
+import Link from 'next/link';
+import { Router, useRouter } from 'next/router';
 
 function blog({ posts }) {
     const router = useRouter();
-    if(router.isFallback) {
-        return <h2>Loading...</h2>
+    if (router.isFallback) {
+        return <h2>Loading...</h2>;
     }
     return (
-    <Layout>
-        <div>
-            <h1>I am a blog</h1>
+        <Layout>
+            <div>
+                <h1>I am a blog</h1>
 
-            {/* Router Example 1 */}
-            {/* <Link href="/about">
+                {/* Router Example 1 */}
+                {/* <Link href="/about">
                 <a>Click to go to the about page</a>
             </Link> */}
 
-            {/* Router Example 2 */}
-            <button onClick={() => router.push("/about")}>Click to go to the about page</button>
-        
-        {posts.map((post) => (
-          <div key={post.id}>
-            <h2>{post.title}</h2>
-            <h3>By {post.author}</h3>
-          </div>
-        ))}
-      </div>
-    </Layout>
-  );
+                {/* Router Example 2 */}
+                <button onClick={() => router.push('/about')}>
+                    Click to go to the about page
+                </button>
+
+                {posts.map((post) => (
+                    <div key={post.id}>
+                        <h2>{post.title}</h2>
+                        <h3>By {post.author}</h3>
+                    </div>
+                ))}
+            </div>
+        </Layout>
+    );
 }
 
-    // function timeout(ms) {
-    // }
-    //     return new Promise((resolve) => setTimeout(resolve, ms));
-    //     console.log(ms)
+// function timeout(ms) {
+// }
+//     return new Promise((resolve) => setTimeout(resolve, ms));
+//     console.log(ms)
 
-    // setTimeout(() => {
-    //     console.log('wiating');
-    // }, 3000)
+// setTimeout(() => {
+//     console.log('wiating');
+// }, 3000)
 
-    // await(timeout(3000))
+// await(timeout(3000))
 
-    // const sleep = (milliseconds, apolloClient) => {
-    //     return (
-    //         new Promise((resolve, reject) =>{
-    //             setTimeout(() => {
-    //                 const request = apolloClient.query({
-    //                     query: XXXXXX,
-    //                     variables: {handle: "XXXXXX"}
-    //                 });
-    //                 resolve(request);
-    //             }, milliseconds)
-    //         });
-    //     )
+// const sleep = (milliseconds, apolloClient) => {
+//     return (
+//         new Promise((resolve, reject) =>{
+//             setTimeout(() => {
+//                 const request = apolloClient.query({
+//                     query: XXXXXX,
+//                     variables: {handle: "XXXXXX"}
+//                 });
+//                 resolve(request);
+//             }, milliseconds)
+//         });
+//     )
 
-    // await sleep(3000, apolloClient.)
+// await sleep(3000, apolloClient.)
 
+export async function getStaticProps() {
+    const res = {
+        data: [
+            {
+                id: '123',
+                title: 'My first blog',
+                author: 'Sonny',
+            },
+            {
+                id: '456',
+                title: 'Gems first blog',
+                author: 'Gwen',
+            },
+        ],
+    };
+    const posts = res.data;
 
-    export async function getStaticProps() {
-        const res = {
-            data: [
-                {
-                    id: '123',
-                    title: 'My first blog',
-                    author: 'Sonny'
-                },
-                {
-                    id: '456',
-                    title: 'Gems first blog',
-                    author: 'Gwen'
-                }
-            ]
-        }
-        const posts = res.data
-    
-        return {
-            props: {
-                posts,
-    
-            }
-        }
-    }
+    return {
+        props: {
+            posts,
+        },
+    };
+}
 
-export default blog
+export default blog;
