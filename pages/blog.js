ebc088755b64eb08eccdd52050ea63a278bc5032
Gwenaël Magnenat <g.magnenat@gmail.com>
2021-03-05 01:16:07 +0100

    start to give some style

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,13 @@
-import Link from 'next/link';
-import { Router, useRouter } from 'next/router';  
+import { Router, useRouter } from 'next/router'
 
 function BlogPage({ posts }) {
-    const router = useRouter();
+    const router = useRouter()
     if (router.isFallback) {
-        return <h2>Loading...</h2>;
+        return <h2>Loading...</h2>
     }
     return (
         <div>
-            <h1>I am a blog</h1>
+            <h1 className="text-3xl font-bold">I am a blog</h1>
 
             {/* Router Example 1 */}
             {/* <Link href="/about">
@@ -16,18 +15,21 @@ function BlogPage({ posts }) {
             </Link> */}
 
             {/* Router Example 2 */}
-            <button onClick={() => router.push('/about')}>
+            <button
+                className="focus:outline-none text-white text-sm py-2.5 px-5 rounded-md bg-blue-500 hover:bg-blue-600 hover:shadow-lg my-3"
+                onClick={() => router.push('/about')}
+            >
                 Click to go to the about page
             </button>
 
             {posts.map((post) => (
                 <div key={post.id}>
-                    <h2>{post.title}</h2>
+                    <h2 className="text-2xl my-5">{post.title}</h2>
                     <h3>By {post.author}</h3>
                 </div>
             ))}
         </div>
-    );
+    )
 }
 
 // function timeout(ms) {
@@ -70,15 +72,15 @@ export async function getStaticProps() {
                 author: 'Gwen',
             },
         ],
-    };
+    }
 
-    const posts = res.data;
+    const posts = res.data
 
     return {
         props: {
             posts,
         },
-    };
+    }
 }
 
-export default BlogPage;
+export default BlogPage
